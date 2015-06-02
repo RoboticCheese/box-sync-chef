85b544525360efe2276345788c843d7215f1ddae
Jonathan Hartman <j@p4nt5.com>
2015-06-01 22:30:27 -0700

    Initial commit; freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Box Sync Cookbook CHANGELOG
+===========================
+
+v0.0.1 (2015-06-01)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'box-sync'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures box-sync'
+long_description 'Installs/Configures box-sync'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
