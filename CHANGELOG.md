800db0bb8436ee819624d6349a7831fa14c285e0
Jonathan Hartman <j@p4nt5.com>
2015-06-07 00:35:29 -0700

    Bump version for dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 Box Sync Cookbook CHANGELOG
 ===========================
 
+v?.?.? (????-??-??)
+-------------------
+
 v0.1.0 (2015-06-07)
 -------------------
 - Initial release! Supports OS X and Windows!
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@p4nt5.com'
 license          'apache2'
 description      'Installs the Box Sync app'
 long_description 'Installs the Box Sync app'
-version          '0.1.0'
+version          '0.1.1'
 
 supports         'mac_os_x'
 supports         'windows'
