76e9aed9fe4d367526d3125f95cb1af5cc1778fd
Jonathan Hartman <j@p4nt5.com>
2015-12-10 13:12:52 -0800

    Bump version for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 Box Sync Cookbook CHANGELOG
 ===========================
 
-v?.?.? (????-??-??)
+v1.0.0 (2015-12-10)
 -------------------
 - Convert to Chef custom resources (breaking compatibility with Chef < 12.5)
 - Update package download URLs
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@p4nt5.com'
 license          'apache2'
 description      'Installs the Box Sync app'
 long_description 'Installs the Box Sync app'
-version          '0.1.1'
+version          '1.0.0'
 
 supports         'mac_os_x'
 supports         'windows'
